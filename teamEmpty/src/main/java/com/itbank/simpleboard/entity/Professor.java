a8d6395d00574665deb1db8a76d7e346d33221f2
RightAccept <djfltrhkdeo46@gmail.com>
2024-01-02 18:19:18 +0900

    Entity 생성
    User,Manager, Professor 생성

---
+++ teamEmpty/src/main/java/com/itbank/simpleboard/entity/Professor.java
@@ -0,0 +1,33 @@
+package com.itbank.simpleboard.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@DiscriminatorValue("professor")
+public class Professor {
+    // 교수 번호
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long professor_idx;
+
+    // 교수 강의
+    private String professor_class;
+
+    // 교수 사진
+    private String professor_img;
+
+    @OneToOne
+    @JoinColumn(name="user_idx")
+    private User user_idx;
+
+//    @JoinColumn(name = "major_idx")
+//    private Major major_idx;    // 학과 번호
+
+}
