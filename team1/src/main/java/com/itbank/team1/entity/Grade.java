59bf0c8ad7d4326c6d06fc52194a56907b16939f
hwangminu <hwangminu0@naver.com>
2024-01-02 18:08:53 +0900

    1일차 장학금 / 성적 entity 추가

---
+++ team1/src/main/java/com/itbank/team1/entity/Grade.java
@@ -0,0 +1,33 @@
+package com.itbank.team1.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+// 성적
+public class Grade {
+
+    @Id
+    // 학생번호 (외래키)
+//    @JoinColumn(name = "student_idx")
+//    private Student student_idx;
+
+    // 강의번호 (외래키)
+//    @JoinColumn(name = "lecture_idx")
+//    private Lecture lecture_idx;
+
+    // 학점
+    @Column(name = "grade_score")
+    private String score;
+
+}
