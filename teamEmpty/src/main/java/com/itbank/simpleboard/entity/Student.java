8ca78718438fcad0af16a70cc32e8d46acbc531e
Soeun21 <131752096+Soeun21@users.noreply.github.com>
2024-01-02 18:11:29 +0900

    학생, 상태 엔티티 생성

---
+++ teamEmpty/src/main/java/com/itbank/simpleboard/entity/Student.java
@@ -0,0 +1,36 @@
+package com.itbank.simpleboard.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Student {
+
+    // 학생번호
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long student_idx;
+
+    // 학번
+    private Integer student_num;
+
+    // 학년
+    private Integer student_grade;
+
+    // 장학금아이디
+//    @JoinColumn(name = "schoolarship_idx")
+//    private Schoolarship schoolarship_idx;
+
+    // 기본키
+//    private User user_idx;
+
+    // 지도교수번호
+//    private Professor professor_idx;
+
+//     학과번호
+//    private Major major_idx;
+
+}
