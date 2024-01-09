de6ee0f436888e3cd2642458a1beee1bfbdf00d0
LeeBG <donny1848@gmail.com>
2024-01-09 18:28:49 +0900

    페이지 일부 수정 + 사이드바는 수정 좀 더해야할듯?? HomeController에 테스트용 mapping 추가 + 로고 이미지 추가

--- teamEmpty/src/main/java/com/itbank/simpleboard/controller/HomeController.java
+++ teamEmpty/src/main/java/com/itbank/simpleboard/controller/HomeController.java
@@ -20,9 +20,48 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/test1")
+    public String test1() {
+        return "manager/registerLecture";
+    }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "manager/registerLectureEvaluationStu";
+    }
+
+    @GetMapping("/test3")
+    public String test3() {
+        return "manager/registerMajor";
+    }
+
+    @GetMapping("/test4")
+    public String test4() {
+//        int n = 2/0;
+        return "manager/studentStatManage";
+    }
+
     @GetMapping("/list")
     public String list(){
         return "student/lectureList";
+    }
+
+    @GetMapping("/side")
+    public String side(){
+        return "layout/sidebar";
+    }
+
+
+    @GetMapping("/err")
+    public String error() {
+        int n = 10 / 0;
+        System.out.println(n);
+        return "home";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "common/register";
     }
 
 }
