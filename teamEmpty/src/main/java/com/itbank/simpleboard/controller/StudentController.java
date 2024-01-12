e7553a0a063afd94b7cb29fef1645aa0c59dd165
LeeBG <donny1848@gmail.com>
2024-01-12 18:47:22 +0900

    수강평가 이거 다시해야한다.

--- teamEmpty/src/main/java/com/itbank/simpleboard/controller/StudentController.java
+++ teamEmpty/src/main/java/com/itbank/simpleboard/controller/StudentController.java
@@ -1,12 +1,17 @@
 package com.itbank.simpleboard.controller;
 
+import com.itbank.simpleboard.dto.LectureDto;
+import com.itbank.simpleboard.dto.LectureListDto;
 import com.itbank.simpleboard.dto.UserDTO;
 import com.itbank.simpleboard.entity.*;
+import com.itbank.simpleboard.service.EnrollmentService;
+import com.itbank.simpleboard.service.LectureService;
 import com.itbank.simpleboard.service.SituationServive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,26 +24,43 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StudentController {
 
-    //  수강신청
-    private final SituationServive situationServive;
 
+    private final SituationServive situationServive;
+    //  수강신청
+    private final EnrollmentService enrollmentService;
+
+    private final LectureService lectureService;
     @GetMapping("/enroll")
     public ModelAndView enrollList(HttpSession session) {
         ModelAndView mav = new ModelAndView("home");
-        // 수강신청 리스트
         UserDTO dto = (UserDTO)session.getAttribute("user");
+        List<LectureDto> dtos = lectureService.selectAll();
+
 
         if(dto != null && dto.getRole().toString().equals("학생")){
-            mav.setViewName("student/mysituation");
+            mav.setViewName("student/enrollment");
+            mav.addObject("list",dtos);
         }
         return mav;
     }
     
     // 수강 신청프로세스
     @PostMapping("/enroll")
-    public Situation enrollPro() {
-        // 이거 그냥 학생이랑
-        return null;
+    public String enrollPro(LectureDto dto, HttpSession session) {
+        Long lectureIdx = dto.getIdx();
+        UserDTO user = (UserDTO)session.getAttribute("user");
+        if(user != null && user.getRole().toString().equals("학생")){
+            // 학생 서비스에서 학생을 받아서 수강신청 로직을 실행해야한다.
+//            StudentDto dto =
+        }
+        Long userIdx = user.getIdx();
+//        lectureService.enroll(lectureIdx,userIdx);
+        return "home";
     }
+
+//    @GetMapping("/enrollDetail/{idx}")
+//    public String enrollDetail(@PathVariable("idx") Long idx) {
+//        return "home";
+//    }
 
 }
--- teamEmpty/src/main/java/com/itbank/simpleboard/service/LectureService.java
+++ teamEmpty/src/main/java/com/itbank/simpleboard/service/LectureService.java
@@ -1,14 +1,34 @@
 package com.itbank.simpleboard.service;
 
+import com.itbank.simpleboard.dto.LectureDto;
+import com.itbank.simpleboard.dto.LectureListDto;
+import com.itbank.simpleboard.entity.Enrollment;
+import com.itbank.simpleboard.entity.Lecture;
 import com.itbank.simpleboard.repository.GradeRepository;
 import com.itbank.simpleboard.repository.LectureRepository;
+import com.itbank.simpleboard.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class LectureService {
     private final LectureRepository lectureRepository;
+
+    public List<LectureDto> selectAll() {
+        List<Lecture> lectureList = lectureRepository.findAll();
+        List<LectureDto> dtos = new ArrayList<>();
+        for(Lecture lecture : lectureList){
+            LectureDto dto = new LectureDto(lecture.getIdx(), lecture.getName(), lecture.getIntro(),lecture.getCredit(), lecture.getDay(), lecture.getStart(), lecture.getEnd(), lecture.getType().toString(), lecture.getMaxCount(), lecture.getCurrentCount(), lecture.getSemester(), lecture.getGrade(),lecture.getProfessor().getProfessor_idx(),lecture.getPlan(),lecture.getMajor().getIdx(),lecture.getLectureRoom().getIdx(), lecture.getVisible().toString());
+            dtos.add(dto);
+        }
+        System.out.println("dtos : " + dtos);
+        return dtos;
+    }
+
 }
