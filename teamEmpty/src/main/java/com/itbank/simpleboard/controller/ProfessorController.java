22bb4a24af6048633c1829bdbec1c101060dcee5
RightAccept <djfltrhkdeo46@gmail.com>
2024-01-15 23:42:23 +0900

    1. ProfessorController의 PostMapping을 Ajax를 위한 PutMapping으로 완전히 대체 2. lectureList.html에 스크립트를 추가하여 검색 결과가 테이블 형태로 표시되도록 작성 3. 존재하는 학년만 검색 목록에 표시하도록 ProfessorController의 GetMapping과 lectureList.html의 학년 검색 <option> 수정

--- teamEmpty/src/main/java/com/itbank/simpleboard/controller/ProfessorController.java
+++ teamEmpty/src/main/java/com/itbank/simpleboard/controller/ProfessorController.java
@@ -17,15 +17,6 @@ import java.util.stream.Collectors;
 public class ProfessorController {
     private final ProfessorService professorService;
 
-    // 처음엔 RequestMapping으로 Get, Post 모두 현재 2번 방식으로 처리하려고 했음
-    // 그런데 돌려보니, 처음 Get에서 2300 밀리초 이상의 시간이 걸리고, 검색 역시 비슷한 시간이 걸렸음
-    // 그래서 쿼리를 두 번 내보내지 않기 위해 현재 1번 방식의 메서드를 작성했고, 그 결과 1300 밀리초로 시간이 크게 단축됐음
-    // 하지만 문제점이, List를 뽑아온 상태로 MajorList를 추출하다 보니, 그 다음 major 목록이 제대로 출력되지 않음
-    // ex) 처음 Get에서는 9개의 전공 모두 출력 -> 신경과 검색 -> 목록이 나온 후 전공 선택 창에 "신경과"만 존재
-
-    // 1월 15일
-    // 쿼리가 2번 나가면 시간이 오래 걸리는 것 같아서 쿼리를 1번만 날리기 위해 Ajax를 사용하기 위해 코드 작성
-
     @GetMapping("/lectureList") // 강의 목록
     public String lectureList(Model model, LectureSearchCondition condition) {
         long startTime = System.currentTimeMillis();
@@ -37,6 +28,12 @@ public class ProfessorController {
                 .distinct()
                 .collect(Collectors.toList()));
 
+        model.addAttribute("GradeList", lectureDtoList.stream()
+                .map(ProfessorLectureDto::getGrade)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList()));
+
         // LectureDtoList를 Model에 추가
         model.addAttribute("LectureList", lectureDtoList);
 
@@ -46,23 +43,6 @@ public class ProfessorController {
         System.out.println("쿼리 실행 시간: " + elapsedTime + " 밀리초");
         return "professor/lectureList";
     }
-
-//    @PostMapping("/lectureList") // 강의 목록
-//    public String lectureList2(Model model, LectureSearchCondition condition) {
-//        long startTime = System.currentTimeMillis();
-//        model.addAttribute("LectureList", professorService.getLectureDtoList(condition));
-//        model.addAttribute("MajorList", professorService.findAllMajorNames());
-//        model.addAttribute("typeSelect", condition.getType());
-//        model.addAttribute("yearSelect", condition.getYear());
-//        model.addAttribute("semesterSelect", condition.getSemester());
-//        model.addAttribute("gradeSelect", condition.getGrade());
-//        model.addAttribute("majorSelect", condition.getMajor());
-//        long endTime = System.currentTimeMillis();
-//        long elapsedTime = endTime - startTime;
-//
-//        System.out.println("쿼리 실행 시간: " + elapsedTime + " 밀리초");
-//        return "professor/lectureList";
-//    }
 
     @ResponseBody
     @PutMapping("/lectureList/data")
