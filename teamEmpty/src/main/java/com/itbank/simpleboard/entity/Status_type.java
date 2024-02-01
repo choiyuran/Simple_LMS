bf7e25d34aaba66ccd50f023e5b234564d846294
hwangminu <hwangminu0@naver.com>
2024-02-01 15:20:33 +0900

    수정

--- teamEmpty/src/main/java/com/itbank/simpleboard/entity/Status_type.java
+++
@@ -1,7 +0,0 @@
-package com.itbank.simpleboard.entity;
-
-public enum Status_type {
-    재학, 일반휴학, 군휴학, 졸업, 편입, 군휴학신청, 일반휴학신청
-
-}
-
--- teamEmpty/src/main/java/com/itbank/simpleboard/entity/User_role.java
+++
@@ -1,5 +0,0 @@
-package com.itbank.simpleboard.entity;
-
-public enum User_role {
-    교직원, 교수, 학생
-}
