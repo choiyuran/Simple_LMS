13808291bc0047da87c5ba5f4cb418e7c0b8a9dc
RightAccept <djfltrhkdeo46@gmail.com>
2024-02-14 16:43:04 +0900

    GradeRepository - 사용하지 않는 import 제거
    professorModify.html
    - 태그의 >가 지워져서 추가

--- teamEmpty/src/main/java/com/itbank/simpleboard/repository/GradeRepository.java
+++ teamEmpty/src/main/java/com/itbank/simpleboard/repository/GradeRepository.java
@@ -1,6 +1,5 @@
 package com.itbank.simpleboard.repository;
 
-import com.itbank.simpleboard.entity.Evaluation;
 import com.itbank.simpleboard.entity.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
