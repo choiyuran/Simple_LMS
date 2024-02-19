d733b99646e099dec7828a6be619456727192d64
RightAccept <djfltrhkdeo46@gmail.com>
2024-02-19 20:57:13 +0900

    인터셉터 주석 해제

--- teamEmpty/src/main/java/com/itbank/simpleboard/config/MvcConfig.java
+++ teamEmpty/src/main/java/com/itbank/simpleboard/config/MvcConfig.java
@@ -1,6 +1,6 @@
 package com.itbank.simpleboard.config;
 
-//import com.itbank.simpleboard.interceptor.AuthInterceptor;
+import com.itbank.simpleboard.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
@@ -11,12 +11,12 @@ import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer { // 로그인 안돼있으면
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new AuthInterceptor())
-//                .addPathPatterns("/**")
-//                .excludePathPatterns("/css/**", "/js/**", "/excelForm/**", "/img/**", "/scss/**", "/vendor/**", "/", "/login", "/manager/calendar", "/findUserByUser_idAndEmail");
-//    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/js/**", "/excelForm/**", "/img/**", "/scss/**", "/vendor/**", "/", "/login", "/manager/calendar", "/findUserByUser_idAndEmail");
+    }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
