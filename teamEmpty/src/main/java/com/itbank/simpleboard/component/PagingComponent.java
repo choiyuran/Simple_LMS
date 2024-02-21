8355bb911c7961c6c8e706314b02b71995ce239f
choiyuran <133099209+choiyuran@users.noreply.github.com>
2024-02-21 18:46:02 +0900

    페이징 진행 중

---
+++ teamEmpty/src/main/java/com/itbank/simpleboard/component/PagingComponent.java
@@ -0,0 +1,17 @@
+package com.itbank.simpleboard.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class PagingComponent {
+    public static final int MAX_PAGE = 5;
+
+    public int calculateStart(int pageNumber) {
+        return (pageNumber / MAX_PAGE) * MAX_PAGE + 1;
+    }
+
+    public int calculateEnd(int totalPages, int start) {
+        return (totalPages == 0) ? 1 : (start + (MAX_PAGE - 1) < totalPages ? start + (MAX_PAGE - 1) : totalPages);
+    }
+}
