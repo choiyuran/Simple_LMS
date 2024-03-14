f31a6833e16a623359cf9665afaff2b71c5f8c3e
RightAccept <djfltrhkdeo46@gmail.com>
2024-03-14 15:59:08 +0900

    - ProfessorRepository, ProfessorService에서 사용하지 않는 메서드 제거 - professor의 enterGrade.html 삭제

--- teamEmpty/src/main/java/com/itbank/simpleboard/repository/professor/ProfessorRepository.java
+++ teamEmpty/src/main/java/com/itbank/simpleboard/repository/professor/ProfessorRepository.java
@@ -1,24 +1,13 @@
 package com.itbank.simpleboard.repository.professor;
 
-import com.itbank.simpleboard.dto.ProfessorUserDto;
-import com.itbank.simpleboard.dto.ProfessorListDto;
 import com.itbank.simpleboard.entity.Major;
 import com.itbank.simpleboard.entity.Professor;
-import com.itbank.simpleboard.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProfessorRepository extends JpaRepository<Professor, Long>, ProfessorRepositoryCustom {
-    Professor findByUser(User user);
-
-    @Query("SELECT m.name FROM Major m")
-    List<String> findAllMajorNames();
-
     List<Professor> findAllByMajor(Major major);
 
-
     List<Professor> findAllByMajorName(String majorName);
-
 }
